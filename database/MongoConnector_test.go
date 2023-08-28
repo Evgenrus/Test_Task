755c90c0facfcd46b5c8463b90d7a8c2f15e8afd
Евгений <evgenrus257@yandex.ru>
2023-08-28 21:33:37 +0700

    added GetRecordByGuid(string) (*dto.TokenRecord, error) and test for this method

--- database/MongoConnector_test.go
+++ database/MongoConnector_test.go
@@ -61,6 +61,31 @@ func TestMongoConnector_RefreshHash(t *testing.T) {
 	}
 }
 
+func TestMongoConnector_GetRecordByGuid(t *testing.T) {
+	conn, cleanup := createConnector(t)
+	defer cleanup()
+
+	_, err := conn.Collection.InsertOne(conn.ctx, bson.M{"_id": "1234", "refresh_hash": []byte("1234token")})
+	assert.NoError(t, err)
+
+	cases := []struct {
+		name     string
+		guid     string
+		hash     []byte
+		expected bool
+	}{
+		{"Finding existing user", "1234", []byte("1234token"), true},
+		{"Finding not existing user", "9876", []byte("notExists"), false},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			_, err := conn.GetRecordByGuid(testCase.guid)
+			assert.Equal(t, err == nil, testCase.expected)
+		})
+	}
+}
+
 func createConnector(t *testing.T) (*MongoConnector, func()) {
 	conn, err := CreateMongoConnector("mongodb://localhost:27017", "test_user", "userTokens")
 	assert.NoError(t, err)
