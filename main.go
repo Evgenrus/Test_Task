3b24910250ba2da41e2c53e5e4a6234e75373951
Евгений <evgenrus257@yandex.ru>
2023-08-30 03:09:16 +0700

    Added config

--- main.go
+++ main.go
@@ -4,9 +4,16 @@ import (
 	"Test_Task_Jwt/database"
 	"Test_Task_Jwt/server"
 	"Test_Task_Jwt/services"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 )
+
+func init() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Print("Cannot load config")
+	}
+}
 
 func main() {
 
